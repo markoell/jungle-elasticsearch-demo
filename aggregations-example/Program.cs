1c6ffdf103ea6b10c9e56d8e8ec7b8007d3616cf
Marco Köllner <git-client@fcafb.de>
2022-02-23 19:00:32 +0100

    Connect ES

--- aggregations-example/Program.cs
+++ aggregations-example/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Nest;
+
+var connectionSettings = new ConnectionSettings(new Uri("http://localhost:9200"))
+        .DefaultIndex("default")
+        //.BasicAuthentication("elastic", "gMFJbCqZR6HFlArw21bd")
+        .ApiKeyAuthentication("ARk2J38BO4QHpYt-7Df1", "GkIpG_uNQtyHX296e7I2WA")
+        .DisableDirectStreaming();
+
+var client = new ElasticClient(connectionSettings);
+
+var result = await client.SearchAsync<Rootobject>(s => s.Index("products").Query(q => q.MatchAll()));
+Console.WriteLine(result.DebugInformation);
---
+++ aggregations-example/Rootobject.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+public record Rootobject
+{
+    public string name { get; set; }
+    public int price { get; set; }
+    public int in_stock { get; set; }
+    public int sold { get; set; }
+    public string[] tags { get; set; }
+    public string description { get; set; }
+    public bool is_active { get; set; }
+    public string created { get; set; }
+}
